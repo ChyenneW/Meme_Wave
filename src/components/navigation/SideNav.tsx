af2046a5b86b88a01843c9d221d8d45c3a0c1f2e
ChyenneW <williamscheyenne771@gmail.com>
2023-10-30 23:32:21 -0500

    Import IconHoverEffect to navigation menu links

--- src/components/navigation/SideNav.tsx
+++ src/components/navigation/SideNav.tsx
@@ -6,34 +6,43 @@ import {
   VscSignIn,
   VscSignOut,
 } from "react-icons/vsc";
+import { IconHoverEffect } from "../IconHoverEffect";
 
 export function SideNav() {
   return (
     <nav>
       <ul className="my-5">
         <li className="mb-2">
-          <span className="flex items-center gap-2">
-            <VscAccount className="h-8 w-8" />
-            <span className="text-3xl">PROFILE</span>
-          </span>
+          <IconHoverEffect>
+            <span className="flex items-center gap-2">
+              <VscAccount className="h-8 w-8" />
+              <span className="text-3xl">PROFILE</span>
+            </span>
+          </IconHoverEffect>
         </li>
         <li className="mb-2">
-          <span className="flex items-center gap-2">
-            <VscExtensions className="h-8 w-8" />
-            <span className="text-3xl">DASHBOARD</span>
-          </span>
+          <IconHoverEffect>
+            <span className="flex items-center gap-2">
+              <VscExtensions className="h-8 w-8" />
+              <span className="text-3xl">DASHBOARD</span>
+            </span>
+          </IconHoverEffect>
         </li>
         <li className="mb-2">
-          <span className="flex items-center gap-2">
-            <VscGraph className="h-8 w-8" />
-            <span className="text-3xl">ACTIVITY</span>
-          </span>
+          <IconHoverEffect>
+            <span className="flex items-center gap-2">
+              <VscGraph className="h-8 w-8" />
+              <span className="text-3xl">ACTIVITY</span>
+            </span>
+          </IconHoverEffect>
         </li>
         <li className="mb-2">
-          <span className="flex items-center gap-2">
-            <VscCommentDiscussion className="h-8 w-8" />
-            <span className="text-3xl">REPORT</span>
-          </span>
+          <IconHoverEffect>
+            <span className="flex items-center gap-2">
+              <VscCommentDiscussion className="h-8 w-8" />
+              <span className="text-3xl">REPORT</span>
+            </span>
+          </IconHoverEffect>
         </li>
       </ul>
       <div>Light/Dark</div>

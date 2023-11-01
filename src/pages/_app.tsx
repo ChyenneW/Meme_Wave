f37d1478c871d3fd36154f648221d687bf225776
ChyenneW <williamscheyenne771@gmail.com>
2023-10-31 19:57:12 -0500

    Link components to build report page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import Head from "next/head";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
-import Head from "next/head";
+
 import { SideNav } from "~/components/navigation/SideNav";
 
 const MyApp: AppType<{ session: Session | null }> = ({
--- src/pages/report.tsx
+++ src/pages/report.tsx
@@ -1,7 +1,11 @@
+import ContactForm from "~/components/contact/ContactForm";
+import ContactGreeting from "~/components/contact/ContactGreeting";
+
 export default function Report() {
   return (
-    <>
-      <div>Report</div>
-    </>
+    <div className="flex h-full flex-grow gap-2 p-4">
+      <ContactGreeting />
+      <ContactForm />
+    </div>
   );
 }
